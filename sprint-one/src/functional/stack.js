d10d138b58aee0be01c7234ce346be2bbd1e69e1
Tina Shih <tinaemilyshih@gmail.com>
2019-12-11 15:20:54 -0800

    functional-stack.js completed

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,16 +3,38 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
-
+  // create a counter variable, and start it at 0
+  var counter = 0;
   // Implement the methods below
   someInstance.push = function(value) {
+    // add to storage
+    // [counter] = value. Output: {0: value, 1: value1}
+    someInstance[counter] = value;
+    // counter++;
+    counter++;
   };
 
+
+  // deletes the last item
   someInstance.pop = function() {
+    var lastItem = someInstance[counter - 1]; //someInstance[1]
+    // delete storage[counter]
+    delete someInstance[counter - 1];
+    // counter--
+    counter--;
+    // return lastItem
+    return lastItem;
   };
 
   someInstance.size = function() {
+    // if the stack is empty
+    if (counter <= 0) {
+      return 0;
+    } else if (counter > 0) {
+    // otherwise,
+      return counter;
+    }
   };
-
   return someInstance;
+
 };
