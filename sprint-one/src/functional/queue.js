6574214ea2c1736f6edb2d9410fa427d3a31eb72
Tina Shih <tinaemilyshih@gmail.com>
2019-12-11 15:48:45 -0800

    functional-queue.js completed

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -4,15 +4,40 @@ var Queue = function() {
   // Use an object with numeric keys to store values
   var storage = {};
 
+  var counter = 0;
+  var firstItem = 0;
+  var finalSize = 0;
+
+  // example: {0: value1, 1: value2} | counter = 0 | counter++ | Output counter = 1
+  // someInstance.dequeue(delete obj[0]) Output {1: value2} | var initial = counter | initial-- | initial = 1 - 1 = 0
+  // someInstance.size return initial
+
   // Implement the methods below
 
+  // add to the back of the obj
   someInstance.enqueue = function(value) {
+    someInstance[counter] = value;
+    counter++;
+    finalSize++;
   };
+
+  // remove the first key value pair in obj
 
   someInstance.dequeue = function() {
+    var lastQueue = someInstance[firstItem];
+    delete someInstance[firstItem];
+    firstItem++;
+    finalSize--;
+    return lastQueue;
   };
 
+  // return the length of the obj
   someInstance.size = function() {
+    if(finalSize <= 0) {
+      return 0;
+    } else {
+      return finalSize;
+    }
   };
 
   return someInstance;
