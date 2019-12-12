270ed71b3915430e9cba9f0b9ee0bc31e420b584
Tina Shih <tinaemilyshih@gmail.com>
2019-12-11 16:48:38 -0800

    functional-shared-stack.js error with counter not resetting

--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,39 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var newStack = {};
+
+  _.extend(newStack, stackMethods);
+  return newStack;
+
 };
 
 var stackMethods = {};
 
+var counter = 0;
 
+stackMethods.push = function(value) {
+  this[counter] = value;
+  counter++;
+  console.log('push', counter);
+};
+
+stackMethods.pop = function() {
+  var lastItem = this[counter - 1];
+  delete this[counter - 1];
+  counter--;
+  console.log('pop', counter);
+  console.log('lastItem', lastItem);
+  return lastItem;
+};
+
+stackMethods.size = function() {
+  if (counter <= 0) {
+    return 0;
+  } else {
+    console.log('final', counter);
+    return counter;
+  }
+};
+
+//Stack()
