d6e9858fc086e64e3454e9844a5d2afe0c4db1a1
Tina Shih <tinaemilyshih@gmail.com>
2019-12-13 15:05:46 -0800

    Linked List: Pseudocode in progress

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -3,7 +3,20 @@ var LinkedList = function() {
   list.head = null;
   list.tail = null;
 
-  list.addToTail = function(value) {
+  // variable
+
+  // list {head: null, tail: null}
+
+  //
+  list.addToTail = function(value) { // {head: null, tail: null} // value = 12
+    // create Node(12)  // {value: 12, next: null}
+
+    // If (list.head = null and list.tail = null) // true
+    //// head points to Node(12) // {head: Node(12), Node(12),tail: null}
+    //// tail points to Node(12) // {head: Node(12), Node(12),tail: Node(12)}
+    // else  // Node(99)
+    ////
+
   };
 
   list.removeHead = function() {
